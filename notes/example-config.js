45d1859f80b8953a1ad9a70d7d3a1a4da84f61eb
tisAliG <ali.groening@yahoo.com>
2015-09-11 18:09:36 -0700

    Delete account info file that was created. Started example project config file.

---
+++ notes/example-config.js
@@ -0,0 +1,56 @@
+/*
+ * This is the configuration file that is needed to create the real 
+ * documentation such as the pattern library. 
+ *
+ * When a new project is created this file will need to be updated with the
+ * correct paths from your project.
+ *
+ */
+{
+    //The pattern library will be created and include documentation for colors,
+    //typography, UI elements and more complex components. This option is required.
+    sources: {
+    //include path to a CSS/SCSS/LESS file that only includes all of the breakpoints
+		breakpoints: [ 
+            [ '0', '435' ], 
+            [ '640', '1024' ], 
+            [ '1153' ]
+        ],
+        //include path to JS files
+        modules: 'demo/library/js/modules/*.js',
+        //include path to component files
+        components: 'demo/components/*.html',
+        //include path to CSS/SCSS/LESS file that only includes all of the colors
+    	colors: 'demo/library/styles/global/colors.scss',
+        //include path to CSS/SCSS/LESS file that only includes all typography
+    	typography: 'demo/library/styles/global/typography.scss'
+    },
+    generators: {
+        //environment information will include breakpoints. This is required, but if
+        //do not have a file with all of the breakpoints you may give it an array.  
+        envInfo: {},
+        patternLibrary: {
+            dest: 'demo/documentation/pattern-library',
+            //include path to where the templates will live for the pattern library
+            srcTemplates: 'demo/documentation/pattern-library/sources/templates'
+        }
+    },
+    //ReadMes is an option that will allow you to choose if you want any of the 
+    //project's readme files to be available within the documentation. 
+    readmes: {
+        // adds research readmes if they're the only document available within a folder
+      	//research: true,        
+        // for one particular document
+        frameworks: 'demo/documentation/build/frameworks',
+        workflows: {
+            jira: '',
+            releaseProcess: ''
+        },
+        // for multiple documents in a folder
+      	codestyleguide: {
+      		css: 'demo/documentation/build/codestyle/css.md',
+      		html: 'demo/documentation/codestyle/html.md',
+            javascript: 'demo/documentation/codestyle/javascript.md'
+       	}
+    } 
+}
