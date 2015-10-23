a4c07f6e443de9f9ded5ab9bb897d642fc164e80
Valerie Balcom <vbalcom@sapient.com>
2015-10-22 17:14:14 -0700

    move utility and shared functions across all generators to separate folders

---
+++ generators/pattern-library/index.js
@@ -0,0 +1,62 @@
+var util = require( 'util' )
+    , fs = require( 'fs' )
+    , async = require( 'async' )
+    , render = require( './lib/render' )
+    , parser = require( './lib/parser' )
+    , globber = require( './lib/globber' )
+    , config = require( './example/config.js' )
+    ;
+
+function Generate( options ) {
+    
+    this.configObj = options || config
+    this.parser = parser();
+    
+    this.init();
+};
+// run in the terminal using `node index.js`
+Generate.prototype = {
+        
+    init: function( options ) {
+        
+        this.getFiles();
+    },
+
+    getFiles: function() {
+
+        var sections = this.configObj.sections;
+
+        sections.forEach( function( section ){
+
+            var newFiles = globber( section.files );
+
+            section.files = newFiles;
+        });
+        
+        this.parseFiles();
+    },
+    
+    parseFiles: function() {
+        
+        var sections = this.configObj.sections
+            , self = this
+            ;
+        // must bind to parser to retain scope
+        async.each( sections, this.parser.parseSection.bind( this.parser ), function() {
+            console.log( sections );
+            
+            // set config obj to new state with added data
+            self.configObj.sections = sections;
+            
+            render( self.configObj );
+            
+        });
+    }
+};
+
+// new Generate();
+
+module.exports = function( options ) {
+
+    return new Generate( options );
+};
