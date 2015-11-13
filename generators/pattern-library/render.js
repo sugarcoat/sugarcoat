a65d668baa22991d17f7edd7e880787cfb7457e9
Allison Groening <agroening@sapient.com>
2015-11-13 15:51:28 -0800

    update moveFiles and setupFiles, to use config destination rather than hardcoded path

--- generators/pattern-library/render.js
+++ generators/pattern-library/render.js
@@ -222,12 +222,15 @@ Render.prototype = {
     // setupStyles: reads in the style files for the pattern library and adds them to the project so we can access them
     moveFile: function( oldFile, newFile, folder ) {
 
+        var destination = this.dest;
+        // console.log('dest', typeof this.dest);
+
         //read in furtive CSS
         fs.readFile( oldFile, 'utf8', function( err, data ) {
 
             if ( err ) throw err;
 
-            var dir = path.join( process.cwd(), 'documentation/pattern-library/' );
+            var dir = path.join( process.cwd(), destination ); //second option should be settings: dest
             dir = path.join( dir, folder );
 
             if ( !fs.existsSync( dir ) ) {
@@ -248,27 +251,29 @@ Render.prototype = {
 
     setupFiles: function() {
 
+        // console.log('destinationsssssssssss', this.dest);
+
         //furtive file paths
         var furtive = path.join( __dirname, 'templates/styles/furtive.css' );
-        var newFurtive = path.join( process.cwd(), 'documentation/pattern-library/styles/furtive.css' );
+        var newFurtive = path.join( process.cwd(), this.dest + 'styles/furtive.css' );
         //set up furtive.css
         this.moveFile( furtive, newFurtive, 'styles' );
 
         //patt-lib file paths
         var patLib = path.join( __dirname, 'templates/styles/pattern-lib.css' );
-        var newPattLib = path.join( process.cwd(), 'documentation/pattern-library/styles/pattern-lib.css' );
+        var newPattLib = path.join( process.cwd(), this.dest + 'styles/pattern-lib.css' );
         //set up furtive.css
         this.moveFile( patLib, newPattLib, 'styles' );
 
         //furtive file paths
         var prismCSS = path.join( __dirname, 'templates/styles/prism.css' );
-        var newPrismCSS = path.join( process.cwd(), 'documentation/pattern-library/styles/prism.css' );
+        var newPrismCSS = path.join( process.cwd(), this.dest + 'styles/prism.css' );
         //set up furtive.css
         this.moveFile( prismCSS, newPrismCSS, 'styles' );
 
         //prism.js file paths
         var prismJS = path.join( __dirname, 'templates/js/prism.js' );
-        var newPrismJS = path.join( process.cwd(), 'documentation/pattern-library/js/prism.js' );
+        var newPrismJS = path.join( process.cwd(), this.dest + 'js/prism.js' );
         //set up prism.js
         this.moveFile( prismJS, newPrismJS, 'js' );
     }
