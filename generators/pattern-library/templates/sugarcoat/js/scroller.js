9dc0ca2a998c4771d4aea0f0c9b64bfeb4ebda45
Valerie Balcom <vbalcom@sapient.com>
2016-04-22 16:01:08 -0700

    convert scroll function to javascript only for scroll event listener clean up active css for nav scroll event

--- generators/pattern-library/templates/sugarcoat/js/scroller.js
+++ generators/pattern-library/templates/sugarcoat/js/scroller.js
@@ -1,74 +1,110 @@
 // http://jsfiddle.net/mekwall/up4nu/ modified to keep track of two sets fo nav items
 // Cache selectors
-var lastId, lastId2, topMenu = $('#sugar-nav'),
-    // All list items
-    menuItems = topMenu.find('a'),
-    primaryItems = topMenu.find('.sugar-nav-item'),
-    secondaryItems = topMenu.find('.sugar-nav-subitem'),
-    // Anchors corresponding to menu items
-    scrollPrimaryItems = primaryItems.map(function() {
-        var item = $($(this).attr('href'));
-        if (item.length) {
-            return item;
-        }
-    }),
-    scrollSecondaryItems = secondaryItems.map(function() {
-        var item = $($(this).attr('href'));
-        if (item.length) {
-            return item;
-        }
-    });
 
-// Bind click handler to menu items
-// so we can get a fancy scroll animation
-menuItems.click(function(e) {
-    var href = $(this).attr('href'),
-        offsetTop = href === '#' ? 0 : $(href).offset().top + 1;    
-    
-    $('html, body').stop().animate({
-        scrollTop: offsetTop
-    }, 300);
-    e.preventDefault();
-});
+var lastId
+    , lastId2
+    , primaryItems = document.querySelectorAll( '.sugar-nav-item' )
+    , secondaryItems = document.querySelectorAll( '.sugar-nav-subitem' )
+    ;
 
-// Bind to scroll
-$(window).scroll(function() {
-    // Get container scroll position
-    var fromTop = $(this).scrollTop();
-    // Get id of current scroll item
-    var cur = scrollPrimaryItems.map(function() {
-        if ($(this).offset().top < fromTop) {
+function getHrefValues( obj ) {
 
-            return this;
-        }
-    });
-    var cur2 = scrollSecondaryItems.map(function() {
-        if ($(this).offset().top < fromTop) {
-            return this;
-        }
-    });
+    var hrefArray = [];
 
-    // Get the id of the current element
-    cur = cur[cur.length - 1];
-    cur2 = cur2[cur2.length - 1];
-
-    var id = cur && cur.length ? cur[0].id : '';
-    var id2 = cur2 && cur2.length ? cur2[0].id : '';
-
-
-    if (lastId !== id) {
-        lastId = id;
-        // Set/remove active class
-        primaryItems.parent().removeClass('active')
-            .end()
-            .filter('[href="#' + id + '"]')
-            .parent().addClass('active');
-    }
-    if (lastId2 !== id2) {
-        lastId2 = id2;
-        // Set/remove active class
-        secondaryItems.parent().removeClass('active');
-        secondaryItems.filter( '[href="#' + id2 + '"]').parent().addClass( 'active' );
+    for ( var i = 0; i < obj.length; i++ ) {
         
+        var href = obj[ i ].getAttribute( 'href' );
+        
+        hrefArray[ i ] = document.querySelectorAll( href )[ 0 ];
+    }
+
+    return hrefArray;
+}
+
+var primaryScrollItems = getHrefValues( primaryItems );
+var secondaryScrollItems = getHrefValues( secondaryItems );
+
+// var lastId, lastId2, topMenu = $('#sugar-nav'),
+//     // All list items
+//     menuItems = topMenu.find('a'),
+//     primaryItems = topMenu.find('.sugar-nav-item'),
+//     secondaryItems = topMenu.find('.sugar-nav-subitem'),
+//     // Anchors corresponding to menu items
+//     scrollPrimaryItems = primaryItems.map(function() {
+//         var item = $($(this).attr('href'));
+//         if (item.length) {
+//             return item;
+//         }
+//     }),
+//     scrollSecondaryItems = secondaryItems.map(function() {
+//         var item = $($(this).attr('href'));
+//         if (item.length) {
+//             return item;
+//         }
+//     });
+//
+// // Bind click handler to menu items
+// // so we can get a fancy scroll animation
+// menuItems.click(function(e) {
+//     var href = $(this).attr('href'),
+//         offsetTop = href === '#' ? 0 : $(href).offset().top + 1;
+//
+//     $('html, body').stop().animate({
+//         scrollTop: offsetTop
+//     }, 300);
+//     e.preventDefault();
+// });
+
+window.addEventListener( 'scroll', function( e ) {
+    
+    var currentPrimary = 0
+        , currentSecondary = 0
+        ;
+    
+    for ( var j = 0; j < primaryScrollItems.length; j++ ) {
+        
+        var primaryOffset = primaryScrollItems[ j ].getBoundingClientRect().top - 5;        
+        
+        if ( primaryOffset < 0 ) {
+
+            currentPrimary = j;
+
+        }
+        else {
+            break;
+        }
+    }
+    for ( var k = 0; k < secondaryScrollItems.length; k++ ) {
+
+        var secondaryOffset = secondaryScrollItems[ k ].getBoundingClientRect().top - 5;        
+        
+        if ( secondaryOffset < 0 ) {
+
+            currentSecondary = k;
+        }
+        else {
+            break;
+        }
+    }
+    
+    if ( lastId !== currentPrimary ) {
+        
+        if ( primaryItems[ lastId ]) {
+            
+            primaryItems[ lastId ].classList.remove( 'active' );
+        }
+        primaryItems[ currentPrimary ].classList.add( 'active' );
+        
+        lastId = currentPrimary;
+    }
+    if ( lastId2 !== currentSecondary ) {
+        
+        if ( secondaryItems[ lastId2 ]) {
+            
+            secondaryItems[ lastId2 ].classList.remove( 'active' );
+        }
+        secondaryItems[ currentSecondary ].classList.add( 'active' );
+        
+        lastId2 = currentSecondary;
     }
 });
