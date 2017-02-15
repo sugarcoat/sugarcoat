ba8b9cb2f60c992312a4d84d2f1e2e1e5664304a
Ali Groening <agroening@sapient.com>
2017-02-15 15:00:19 -0800

    removed example site

--- site/index.js
+++
@@ -1,7 +0,0 @@
-var sugarcoat = require( 'sugarcoat' );
-
-var config = require( './documentation/config.js' );
-
-sugarcoat( config ).then( function( data ) {
-    console.log('finished!', data);
-});
