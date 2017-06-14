52b4983884d838515d5858a5ab7c5eb3055a7357
Ali Groening <agroening@sapient.com>
2017-06-14 16:57:28 -0700

    create index test file, and cleanup parser and render of unnecessary comments

--- test/index.js
+++ test/index.js
@@ -1,15 +1,71 @@
 'use strict';
 
 var assert = require( 'chai' ).assert;
-var fs = require( 'fs-extra' );
 
-var parser = require( '../lib/parser' );
 var sugarcoat = require( '../lib/index' );
 
 suite( 'Glob files', function () {
     // test globfile function in index file
+    // test if sections file path (that includes multiple files) globs the files properly
+
+    test.only( 'Sugarcoat globs our file paths correctly.', function () {
+
+        var globFilesConfig = {
+            dest: './test/sugarcoat',
+            sections: [
+                {
+                    title: 'CSS Files',
+                    files: './test/assert/*.css'
+                }
+            ]
+        };
+
+        sugarcoat( globFilesConfig )
+        .then( data => {
+
+            var fileData = data.sections[ 0 ];
+
+            console.log(fileData);
+
+        }, data => {
+
+            assert.isNotObject( data, 'error is not an obj' );
+        });
+    });
 });
 
 suite( 'Read Sections', function () {
-    // test readsections function in index file
+
+    test( 'Sugarcoat is converting our section file string into an object with the correct data.', function () {
+
+        var readSectionsConfig = {
+            dest: './test/sugarcoat',
+            sections: [
+                {
+                    title: 'Colors File',
+                    files: './test/assert/readSections.css'
+                }
+            ]
+        };
+
+        var sectionFilePath = './test/assert/readSections.css'
+            , sectionFileExt = 'css'
+            , sectionFileSrc = '/**\n *\n * @title Primary Colors\n * @description Client-branded color palette\n * @usage Found only in brand-specific UI elements\n *\n */\n:root {\n    --brand-red: #703030; /* Headlines */\n    --brand-grey: #2F343B; /* Links */\n    --accent-red: #C77966;\n    --accent-grey: #7E827A;\n}'
+            ;
+
+        sugarcoat( readSectionsConfig )
+        .then( data => {
+
+            var fileData = data.sections[ 0 ].files[ 0 ];
+
+            assert.isObject( fileData, 'The data in the files array is an object.' );
+            assert.propertyVal( fileData, 'path', sectionFilePath, 'The sections file path is correct.' );
+            assert.propertyVal( fileData, 'ext', sectionFileExt, 'The sections file ext is correct.' );
+            assert.propertyVal( fileData, 'src', sectionFileSrc, 'The sections file src is correct.' );
+
+        }, data => {
+
+            assert.isNotObject( data, 'error is not an obj' );
+        });
+    });
 });
