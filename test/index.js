6f09c9e3732ac2ff827737842a9d625ffa7af8d0
Ali Groening <agroening@sapient.com>
2017-06-16 16:51:08 -0700

    see if it stil errors out with passing test missing from index

--- test/index.js
+++ test/index.js
@@ -5,47 +5,6 @@ var fs = require( 'fs-extra' );
 
 var sugarcoat = require( '../lib/index' );
 
-suite( 'Index: Glob files', function () {
-
-    test( 'Sugarcoat globs our file paths correctly.', function () {
-
-        var globFilesConfig = {
-            dest: './test/sugarcoat',
-            sections: [
-                {
-                    title: 'CSS Files',
-                    files: [
-                        './test/assert/*.css',
-                        '!./test/assert/parseComment.css'
-                    ]
-                }
-            ]
-        };
-
-        sugarcoat( globFilesConfig )
-        .then( data => {
-
-            var fileData = data.sections[ 0 ].files;
-
-            assert.lengthOf( fileData, 5, 'There are 5 files that glob found.' );
-            assert.propertyVal( fileData[ 3 ], 'path', './test/assert/prefixAssets.css', 'The fourth item in the array is the correct file.' );
-
-        }, data => {
-
-            assert.isString( data, 'error is not a string' );
-        });
-    });
-
-    teardown( done => {
-
-        fs.remove( './test/sugarcoat', err => {
-
-            if ( err ) return console.error( err );
-
-            done();
-        });
-    });
-});
 
 suite( 'Index: Read Sections', function () {
 
@@ -69,18 +28,16 @@ suite( 'Index: Read Sections', function () {
         sugarcoat( readSectionsConfig )
         .then( data => {
 
-            // var fileData = data.sections[ 0 ].files[ 0 ];
+            var fileData = data.sections[ 0 ].files[ 0 ];
 
-            // assert.isObject( fileData, 'The data in the files array is an object.' );
-            // assert.propertyVal( fileData, 'path', sectionFilePath, 'The sections file path is correct.' );
-            // assert.propertyVal( fileData, 'ext', sectionFileExt, 'The sections file ext is correct.' );
-            // assert.propertyVal( fileData, 'src', sectionFileSrc, 'The sections file src is correct.' );
-            console.log('pass');
+            assert.isObject( fileData, 'The data in the files array is an object.' );
+            assert.propertyVal( fileData, 'path', sectionFilePath, 'The sections file path is correct.' );
+            assert.propertyVal( fileData, 'ext', sectionFileExt, 'The sections file ext is correct.' );
+            assert.propertyVal( fileData, 'src', sectionFileSrc, 'The sections file src is correct.' );
 
         }, data => {
 
-            // assert.isString( data, 'error is not a string' );
-            console.log('fail');
+            assert.isString( data, 'error is not a string' );
         });
     });
 
